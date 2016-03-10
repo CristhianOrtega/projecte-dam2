b8434807c2c7f67bedf120984f13c616c72dddbf
CristhianOrtega <cristianroq@gmail.com>
2016-03-10 12:52:22 +0100

    fghji

--- app/src/main/java/com/proyecto/dam2/librosvidal/PantallaPrincipal.java
+++ app/src/main/java/com/proyecto/dam2/librosvidal/PantallaPrincipal.java
@@ -78,7 +78,7 @@ public class PantallaPrincipal extends AppCompatActivity
 
         } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.menuConfig) {
 
         } else if (id == R.id.nav_share) {
 
