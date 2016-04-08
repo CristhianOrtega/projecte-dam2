28ce41ce82f115bc62d5d79c2d6776e314ae0c2c
CristhianOrtega <cristianroq@gmail.com>
2016-04-08 12:25:54 +0200

    Modificat -> Manifest - he afegit el permis de connexions a internet Communications esborrat ara es diu httpconnection i fa login correctament ja

--- app/src/main/java/com/proyecto/dam2/librosvidal/LoginActivity.java
+++ app/src/main/java/com/proyecto/dam2/librosvidal/LoginActivity.java
@@ -5,6 +5,7 @@ import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -311,31 +312,34 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-            HashMap<String,String> map = new HashMap<>();
-            map.put("action","login");
-            map.put("email",mEmail);
-            map.put("password", mPassword);
-            String response = Communications.doPost(map,context);
-            Log.i("COC","Response Login ->"+response);
 
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
+            // --- Login ---------------------------------------------------------------------------
+            String response = "";
+            HashMap<String,String> postParams = new HashMap<>();
+            postParams.put("action","login");
+            postParams.put("email",mEmail);
+            postParams.put("password",mPassword);
+            String url = "http://librosvidal.esy.es/api.php";
 
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
+            HttpConnection request = new HttpConnection(url, postParams,
+                    "login");
+
+            while (!request.isReceived()) {
+                try {
+                    Thread.sleep(250);
+                } catch (InterruptedException e) {
+
                 }
             }
 
-            // TODO: register the new account here.
-            return true;
+            response = request.getResponse();
+
+            Log.i("COC","Login->"+response);
+
+            if (response.equals("true")){ return true; }
+            else{ return false; }
+
+
         }
 
         @Override
