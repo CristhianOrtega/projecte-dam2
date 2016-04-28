dbc0ffd1666380a18abeb43c81612b8163cfc9f6
CristhianOrtega <cristianroq@gmail.com>
2016-04-28 10:34:12 +0200

    image

--- app/src/main/java/com/proyecto/dam2/librosvidal/Activities/EditarPerfil.java
+++ app/src/main/java/com/proyecto/dam2/librosvidal/Activities/EditarPerfil.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -25,6 +26,7 @@ import com.proyecto.dam2.librosvidal.Utils.Image;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.HashMap;
 
 public class EditarPerfil extends AppCompatActivity {
@@ -114,56 +116,6 @@ public class EditarPerfil extends AppCompatActivity {
 
     }
 
-    public void dialogPhoto (View view){
-        dialogPhoto();
-    }
-
-    private void dialogPhoto(){
-        try{
-            final CharSequence[] items = {"Seleccionar de la galería", "Hacer una foto"};
-
-            AlertDialog.Builder builder = new AlertDialog.Builder(this);
-            builder.setTitle("Seleccionar una foto");
-            builder.setItems(items, new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int item) {
-                    switch(item){
-                        case 0:
-                            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-                            intent.setType("image/*");
-                            startActivityForResult(intent, SELECT_IMAGE);
-                            break;
-                        case 1:
-                            startActivityForResult(new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE), TAKE_PICTURE);
-                            break;
-                    }
-
-                }
-            });
-            AlertDialog alert = builder.create();
-            alert.show();
-        } catch(Exception e){}
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        ImageButton imgPhoto = (ImageButton) findViewById(R.id.imageButton);
-        try{
-            if (requestCode == SELECT_IMAGE)
-                if (resultCode == Activity.RESULT_OK) {
-                    Uri selectedImage = data.getData();
-                    uriImage = selectedImage;
-                    imgPhoto.setImageURI(selectedImage);
-                }
-            if(requestCode == TAKE_PICTURE)
-                if(resultCode == Activity.RESULT_OK){
-                    Uri selectedImage = data.getData();
-
-                    imgPhoto.setImageURI(selectedImage);
-                }
-        } catch(Exception e){}
-    }
-
 
     public void editaImatge(){
 
@@ -171,7 +123,13 @@ public class EditarPerfil extends AppCompatActivity {
 
         ImageButton img = (ImageButton) findViewById(R.id.editFotoPerfil);
 
-        Bitmap bmap = ((BitmapDrawable)img.getDrawable()).getBitmap();
+        Bitmap bmap =  null;
+        try {
+            bmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uriImage);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         bmap = Image.createThumbnail(bmap);
         String stringImatge = Image.imageToString(bmap);
@@ -213,5 +171,55 @@ public class EditarPerfil extends AppCompatActivity {
 
         }
 
+    }
+
+    public void dialogPhoto (View view){
+        dialogPhoto();
+    }
+
+    private void dialogPhoto(){
+        try{
+            final CharSequence[] items = {"Seleccionar de la galería", "Hacer una foto"};
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Seleccionar una foto");
+            builder.setItems(items, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int item) {
+                    switch(item){
+                        case 0:
+                            Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                            intent.setType("image/*");
+                            startActivityForResult(intent, SELECT_IMAGE);
+                            break;
+                        case 1:
+                            startActivityForResult(new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE), TAKE_PICTURE);
+                            break;
+                    }
+
+                }
+            });
+            AlertDialog alert = builder.create();
+            alert.show();
+        } catch(Exception e){}
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        ImageButton imgPhoto = (ImageButton) findViewById(R.id.editFotoPerfil);
+        try{
+            if (requestCode == SELECT_IMAGE)
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri selectedImage = data.getData();
+                    uriImage = selectedImage;
+                    imgPhoto.setImageURI(selectedImage);
+                }
+            if(requestCode == TAKE_PICTURE)
+                if(resultCode == Activity.RESULT_OK){
+                    Uri selectedImage = data.getData();
+
+                    imgPhoto.setImageURI(selectedImage);
+                }
+        } catch(Exception e){}
     }
 }
